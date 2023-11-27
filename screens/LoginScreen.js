ce18106eb81e31f00e1baa510023731206a974ea
tabassum2507 <10tabassum2@gmail.com>
2023-11-28 02:41:29 +0530

    backend added login page created

---
+++ screens/LoginScreen.js
@@ -0,0 +1,144 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  KeyboardAvoidingView,
+  TextInput,
+  Pressable
+} from "react-native";
+import React from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+
+const LoginScreen = () => {
+  return (
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
+    >
+      <View style={{ marginTop: 50 }}>
+        <Image
+          source={{
+            uri: "https://seeklogo.com/images/T/threads-logo-E9BA734BF6-seeklogo.com.png?v=638252100960000000",
+          }}
+          style={{ width: 150, height: 100, resizeMode: "contain" }}
+        />
+      </View>
+
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: "center", justifyContent: "center" }}>
+          <Text style={{ fontSize: 17, fontWeight: "bold", marginTop: 25 }}>
+            Login to Your Account
+          </Text>
+        </View>
+
+        <View style={{ marginTop: 40 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              borderColor: "#D0D0D0",
+              borderWidth: 1,
+              paddingVertical: 5,
+              borderRadius: 5,
+            }}
+          >
+            <MaterialIcons
+              name="email"
+              size={24}
+              color="black"
+              style={{ marginLeft: 8, color: "gray" }}
+            />
+            <TextInput
+              placeholder="Enter Your Email"
+              style={{ color: "gray", marginVertical: 10, width: 300 }}
+              placeholderTextColor={"gray"}
+            />
+          </View>
+
+          <View style={{ marginTop: 30 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+                borderColor: "#D0D0D0",
+                borderWidth: 1,
+                paddingVertical: 5,
+                borderRadius: 5,
+              }}
+            >
+              <AntDesign
+                name="lock"
+                size={24}
+                style={{ marginLeft: 8, color: "gray" }}
+              />
+              <TextInput
+                placeholder="Enter Your Password"
+                style={{ color: "gray", marginVertical: 10, width: 300 }}
+                placeholderTextColor={"gray"}
+              />
+            </View>
+          </View>
+
+          <View>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginTop: 12,
+              }}
+            >
+              <Text>Keep me logged in</Text>
+              <Text style={{ fontWeight: "500", color: "#007FFF" }}>
+                Forgot Password
+              </Text>
+            </View>
+          </View>
+
+          <View style={{ marginTop: 45 }} />
+
+          <Pressable
+           
+            style={{
+              width: 200,
+              backgroundColor: "black",
+              padding: 15,
+              marginTop: 40,
+              marginLeft: "auto",
+              marginRight: "auto",
+              borderRadius: 6,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                fontWeight: "bold",
+                fontSize: 16,
+                color: "white",
+              }}
+            >
+              Login
+            </Text>
+          </Pressable>
+
+          <Pressable
+           
+            style={{ marginTop: 10 }}
+          >
+            <Text style={{ textAlign: "center", fontSize: 16 }}>
+              Don't have an account? Sign up
+            </Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
